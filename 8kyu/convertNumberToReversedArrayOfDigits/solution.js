c34ef85bf576cd6f57825d80057e4c5cca50d7ad
kbventures <kmb_x@hotmail.com>
2022-05-13 17:50:49 -0400

    8kyu Number Reversed To Array

---
+++ 8kyu/convertNumberToReversedArrayOfDigits/solution.js
@@ -0,0 +1,37 @@
+/*
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+*/
+
+
+const numbers = 42513;
+
+
+
+
+function digitize(n) {
+
+    let tempString = String(n);
+    let tempArr = []
+
+    for(let i=0; i < tempString.length; i++){
+
+        tempArr.push(Number(tempString[i]));
+        console.log(tempArr)
+    }
+
+    return tempArr.reverse();
+  }
+
+
+  digitize(numbers)
+
+
+
+
+module.exports = digitize;
+
+/*
+function digitize(n) {
+  return String(n).split('').map(Number).reverse()
+}
+*/
