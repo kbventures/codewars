fa2daa14a71114453fbf2429a664495318f9d86f
kbventures <kmb_x@hotmail.com>
2022-06-08 09:07:54 -0600

    7kyu Chain Me

---
+++ 7kyu/chain-me/solution.js
@@ -0,0 +1,22 @@
+
+function chain(input, fs) {
+    // implement the "chain" function
+
+    return fs[1](fs[0](input));
+}
+
+function add(x) {
+    return x + 10;
+}
+
+function mult(x) {
+    return x * 30;
+}
+
+
+
+const result = chain(5.891839987285257, [add, mult])
+console.log(result)
+
+
+//  5.891839987285257 to equal 26.755199618557715
