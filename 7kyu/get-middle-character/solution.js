e7282e38f9ddf95d7266fa5c0555a6833ff8a018
kbventures <kmb_x@hotmail.com>
2022-06-18 13:19:42 -0600

    7kyu get middle character

--- 7kyu/get-middle-character/solution.js
+++ 7kyu/get-middle-character/solution.js
@@ -11,10 +11,11 @@ function getMiddle(s) {
     // Turn string into array so you can extract length
     // If the string lenght is even, divide lenght by 2 and return array index position using the division result and concatenate the array index position with division result + 1
     // If the string.length is odd divide string.lenght by two and round up.  return string at position using the result of this division and round up
-    let splitString = s.split('')
 
-    return splitString.length % 2 === 0 ? splitString[(splitString.length / 2) - 1] + splitString[(splitString.length / 2)] :
-        s[(Math.floor(splitString.length / 2))]
+    function getMiddle(s) {
+
+        return s.length % 2 === 0 ? s[(s.length / 2) - 1] + s[(s.length / 2)] : s[(Math.floor(s.length / 2))]
+    }
 
 }
 
@@ -25,4 +26,4 @@ function getMiddle(s) {
 
 // Code Wars Solution
 
-return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+// return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
