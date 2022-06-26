b46670cad2d60454e513a1ea7926edb9453905b6
kbventures <kmb_x@hotmail.com>
2022-06-26 12:52:37 -0600

    Simple multiplication

---
+++ 8kyu/simple-multiplication/solution.js
@@ -0,0 +1,11 @@
+// Parameter number is an integer
+// Returns number by 8 is even and by 9 if odd
+
+function simpleMultiplication(number) {
+    // your code........
+    // Pseudo code
+    // check if number is odd or even, return *8 if even or *9 otherwise
+    return number % 2 === 0 ? number * 8 : number * 9;
+}
+
+// Example: sipmleMultiplication(2) returns 16
