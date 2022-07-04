a79084d9413558545274ad9d8756097923231462
kbventures <kmb_x@hotmail.com>
2022-07-04 09:57:46 -0600

    5kyu Moving Zeros To The End

---
+++ 5kyu/moving-zeros-to-the-end/solution.js
@@ -0,0 +1,7 @@
+let fruits = ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango']
+let citrus = fruits.slice(1, 3)
+
+// fruits contains ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango']
+// citrus contains ['Orange','Lemon']
+
+console.log(fruits)
