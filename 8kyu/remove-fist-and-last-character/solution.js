7fde134ec53bfde694b70df3b525d4165d6b97f4
kbventures <kmb_x@hotmail.com>
2022-07-07 10:20:03 -0600

    8kyu Remove First And Last Character

---
+++ 8kyu/remove-fist-and-last-character/solution.js
@@ -0,0 +1,11 @@
+//Prep
+//
+// Parameter is a string
+// Return the string but with the first and last letter removed
+// Example red returns e
+// Pseudocode return the string with the first and last letter removed using slice
+
+function removeChar(str) {
+    //You got this!
+    return str.slice(1, -1)
+};
