4171dbd5b5ea6ef0ed1b4facd7acccd588c68863
kbventures <kmb_x@hotmail.com>
2022-08-02 09:48:01 -0600

    7kyu two to one

--- 7kyu/credit-card-mask/solution.js
+++ 7kyu/credit-card-mask/solution.js
@@ -2,3 +2,14 @@
 function maskify(cc) {
     return cc.length < 5 ? cc : cc.slice(-4).padStart(cc.length, '#');
 }
+
+// return masked string
+function maskify(cc) {
+    cc = cc.split("");
+    for (var i = 0; i < cc.length - 4; i++) {
+        cc[i] = "#";
+    }
+
+    cc = cc.join("");
+    return cc
+}
