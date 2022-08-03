a415830377edde851ca92dbf2d38a3cfce5df264
kbventures <kmb_x@hotmail.com>
2022-08-03 13:18:19 -0600

    7kyu regex validate pin code

--- 7kyu/find-the-stray-number/solution.js
+++ 7kyu/find-the-stray-number/solution.js
@@ -5,19 +5,20 @@ function stray(numbers) {
     } else {
         return arr[arr.length - 1];
     }
+}
 
-    function stray(numbers) {
-        for (let i = 0; i < numbers.length; i++) {
-            if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
-                return numbers[i];
-            }
-        }
-    }
+// function stray(numbers) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
+//             return numbers[i];
+//         }
+//     }
+// }
 
-    function stray(numbers) {
-        let max = Math.max(...numbers);
-        let min = Math.min(...numbers);
-        return numbers.filter(i => i === max).length === 1 ? max : min;
-    }
-    stray([1, 1, 1, 1, 2]);  // 2
-    stray([4, 4, 8, 4, 4]);  // 8
+// function stray(numbers) {
+//     let max = Math.max(...numbers);
+//     let min = Math.min(...numbers);
+//     return numbers.filter(i => i === max).length === 1 ? max : min;
+// }
+stray([1, 1, 1, 1, 2]);
+stray([4, 4, 8, 4, 4, 1, 4, 8, 2, 2, 3, 3, 4, 5, 5]);  
