43dd8c1c78fb507c8430ec72c4258ead54be7056
kbventures <kmb_x@hotmail.com>
2022-08-06 20:09:14 -0600

    8kyu find the first non consecutive number

---
+++ 8kyu/find-the-first-non-consecutive-number/solution.js
@@ -0,0 +1,9 @@
+function firstNonConsecutive(arr) {
+    console.log(arr)
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] + 1 !== arr[i + 1]) {
+            return arr[i + 1]
+        }
+    }
+    return null
+}
