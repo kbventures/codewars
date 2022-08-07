c155d65f70fb053c16fe6f7083148ec5d1c4d426
kbventures <kmb_x@hotmail.com>
2022-08-06 20:18:21 -0600

    8kyu get planet name by id

---
+++ 8kyu/get-planet-name-by-id/solution.js
@@ -0,0 +1,12 @@
+function getPlanetName(id) {
+    return {
+        1: 'Mercury',
+        2: 'Venus',
+        3: 'Earth',
+        4: 'Mars',
+        5: 'Jupiter',
+        6: 'Saturn',
+        7: 'Uranus',
+        8: 'Neptune'
+    }[id]
+}
