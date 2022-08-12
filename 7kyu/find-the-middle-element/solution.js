082850cafd215469055e78313cbd80feb859a26d
kbventures <kmb_x@hotmail.com>
2022-08-12 08:25:42 -0600

    7kyu simple fun reverse letter

--- 7kyu/find-the-middle-element/solution.js
+++ 7kyu/find-the-middle-element/solution.js
@@ -2,3 +2,10 @@ function gimme(triplet) {
     const sorted = [...triplet].sort((a, b) => a - b)
     return triplet.indexOf(sorted[1])
 }
+
+// function gimme(a) {
+//     return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+//   }
+
+// Return an array
+
