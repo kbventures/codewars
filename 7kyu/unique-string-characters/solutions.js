84b5e9e7c08a1cca09e145c4e873be36a529134f
kbventures <kmb_x@hotmail.com>
2022-08-28 10:54:28 -0600

    7kyu unique string characters updated for time complexity

--- 7kyu/unique-string-characters/solutions.js
+++ 7kyu/unique-string-characters/solutions.js
@@ -1,3 +1,13 @@
 function solve(a, b) {
     return (a + b).split("").filter(c => !a.includes(c) || !b.includes(c)).join("");
 };
+
+O(n ^ 2)
+
+function solve(a, b) {
+    let setA = new Set(a);
+    let setB = new Set(b);
+    return [...(a + b)].filter(c => !setA.has(c) || !setB.has(c)).join("");
+};
+
+O(n)
