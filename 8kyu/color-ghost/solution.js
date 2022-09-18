253b34bd222ddc94b701cc8b9304b2bf5ed404b5
kbventures <kmb_x@hotmail.com>
2022-09-17 18:00:00 -0600

    7kyu color ghost

---
+++ 8kyu/color-ghost/solution.js
@@ -0,0 +1,4 @@
+var Ghost = function () {
+    // your code goes here
+    this.color = ['white', 'yellow', 'purple', 'red'][Math.floor(Math.random() * 4)] // Will return a integer between 0 and 3
+};
