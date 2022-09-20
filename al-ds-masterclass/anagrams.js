c1ca2147c82d3bf0deb91ba8d6c29f8c546a9240
kbventures <kmb_x@hotmail.com>
2022-09-20 10:21:56 -0600

    kyu7 Convert Hash to An Array

--- al-ds-masterclass/anagrams.js
+++ al-ds-masterclass/anagrams.js
@@ -5,28 +5,59 @@
 // You can assume they are single words, no spaces , no periods, nopunctuations, no numbers, all lower case letters.
 
 
-function validAnagram(string1, string2) {
-    if (string1 === "" && string2 === "") return true;
+// Course Solution: 
+function validAnagram(first, second) {
 
-    let str1 = {}
-    let str2 = {}
-    for (key of string1) {
-        str1[key] ? str1[key]++ : str1[key] = 1;
+
+    if (first.length !== second.length) return false;
+
+    const lookup = {}
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i];
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
     }
 
-    for (key of string2) {
-        str2[key] ? str2[key]++ : str2[key] = 1;
+    console.log(lookup)
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i];
+
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
     }
-
-    for (key in str1) {
-        if (!(key in str2)) return false;
-
-        if (str1[key] !== str2[key]) return false;
-    }
-
     return true;
-
 }
+
+// My solution:
+// function validAnagram(string1, string2) {
+//     if (string1 === "" && string2 === "") return true;
+
+//     let str1 = {}
+//     let str2 = {}
+//     for (key of string1) {
+//         str1[key] ? str1[key]++ : str1[key] = 1;
+//     }
+
+//     for (key of string2) {
+//         str2[key] ? str2[key]++ : str2[key] = 1;
+//     }
+
+//     for (key in str1) {
+//         if (!(key in str2)) return false;
+
+//         if (str1[key] !== str2[key]) return false;
+//     }
+
+//     return true;
+
+// }
+
+
+
+
 
 
 
