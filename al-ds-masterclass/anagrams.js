917ec3cbb5035ea5693cb096af6b0028d2efc101
kbventures <kmb_x@hotmail.com>
2022-09-22 11:52:37 -0600

    7kyu personalising spammy marketing emails

--- al-ds-masterclass/anagrams.js
+++ al-ds-masterclass/anagrams.js
@@ -6,29 +6,29 @@
 
 
 // Course Solution: 
-function validAnagram(first, second) {
+// function validAnagram(first, second) {
 
 
-    if (first.length !== second.length) return false;
+//     if (first.length !== second.length) return false;
 
-    const lookup = {}
-    for (let i = 0; i < first.length; i++) {
-        let letter = first[i];
-        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
-    }
+//     const lookup = {}
+//     for (let i = 0; i < first.length; i++) {
+//         let letter = first[i];
+//         lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+//     }
 
 
-    for (let i = 0; i < second.length; i++) {
-        let letter = second[i];
+//     for (let i = 0; i < second.length; i++) {
+//         let letter = second[i];
 
-        if (!lookup[letter]) {
-            return false;
-        } else {
-            lookup[letter] -= 1;
-        }
-    }
-    return true;
-}
+//         if (!lookup[letter]) {
+//             return false;
+//         } else {
+//             lookup[letter] -= 1;
+//         }
+//     }
+//     return true;
+// }
 
 // My solution:
 // function validAnagram(string1, string2) {
