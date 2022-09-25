7202d05ac03b7c20e5ed6414c08666f776752cab
kbventures <kmb_x@hotmail.com>
2022-09-25 09:44:42 -0600

    8kyu class-classes

---
+++ 8kyu/classy-classes/solution.js
@@ -0,0 +1,12 @@
+class Person {
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    get info() {
+        return `${this.name}s age is ${this.age}`;
+    }
+
+}
