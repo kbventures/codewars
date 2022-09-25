8ffa9fcfc51248975aa8d0d311cae5d784626d7f
kbventures <kmb_x@hotmail.com>
2022-09-25 09:44:42 -0600

    8kyu class-classes

--- 7kyu/odd-even-string-sort/solution.js
+++ 7kyu/odd-even-string-sort/solution.js
@@ -8,3 +8,9 @@ function sortMyString(S) {
         return acc;
     }, "")
 }
+
+// const sortMyString = s => {
+//     let even = s.split('').filter((v, i) => i % 2 === 0).join('')
+//     let odd = s.split('').filter((v, i) => i % 2 !== 0).join('')
+//     return even + ' ' + odd
+// }
