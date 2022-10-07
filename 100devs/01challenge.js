a579798afeaa143fed097979bd0b995f86fc9812
kbventures <kmb_x@hotmail.com>
2022-10-07 15:56:21 -0600

    7kyu jaden casing strings

--- 100devs/01challenge.js
+++ 100devs/01challenge.js
@@ -85,7 +85,42 @@ function fix(str) {
         temp[i] = testing;
     }
 
-    return temp;
+    return temp.join(' ');
 }
 
+// console.log(fix("PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770."))
 console.log(fix("PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770."))
+// console.log(fix("e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs"))
+
+
+
+// Elegant solutions
+// function decodeString(str) {
+//     let convert = {
+//       0: 'O',
+//       1: 'I',
+//       2: 'Z',
+//       3: 'E',
+//       4: 'h',
+//       5: 'S',
+//       6: 'G',
+//       7: 'L',
+//       8: 'B',
+//       9: 'q',
+//     };
+  
+//     return [...str]
+//       .map((x) =>
+//         x in convert
+//           ? convert[x]
+//           : Object.values(convert).includes(x)
+//           ? Object.entries(convert).find((a) => a[1] === x)[0]
+//           : x
+//       )
+//       .join('');
+//   }
+  
+//   console.log(
+//     decodeString("PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770."),
+//     "PRO-TIP #498: IT'S NICE TO SAY HELLO."
+//   );
--- 100devs/03challenge.js
+++ 100devs/03challenge.js
@@ -1,8 +1,28 @@
+// function replaceKeyWithSpaces(str, key) {
+//     let result = str;
+//     key.forEach(e => {
+//         result = result.replace(e, "")
+//     })
+//     return result;
+// }
 
 
-function replaceKeyWithSpaces(str) {
-    let result = str;
-    ['X', 'X', 'X', 'X', 'X'].forEach(e => {
-        result = result.replace(e, "")
-    })
+
+// function decodeStringThree(str) {
+//     return str.replace(/[Space]/g, ' ');
+// }
+
+// console.log(
+//     decodeStringThree(
+//         'e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs'
+//     )
+// );
+
+
+let secretMessage = "e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs"
+
+function decodeStringThree(str) {
+    return str.replace(/[ic]/g, ' ');
 }
+
+console.log(decodeStringThree(secretMessage))
