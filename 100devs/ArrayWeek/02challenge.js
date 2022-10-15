234566811a5b7bdfb6fdcec212a7c78cba024c3c
kbventures <kmb_x@hotmail.com>
2022-10-15 09:11:48 -0600

    100devs Huntober Array Challenge 3

--- 100devs/ArrayWeek/02challenge.js
+++ 100devs/ArrayWeek/02challenge.js
@@ -11,39 +11,52 @@ let myArray = ['abc', 'xyz', 1, 2, 'Hey!']
 
 
 function swapLeft(arr, key) {
-    const index = arr.indexOf(key);
-    const leftVal = arr[index - 1];
+    const indexOfKey = arr.indexOf(key);
+    const leftVal = arr[indexOfKey - 1];
 
-    if (index === 0) return arr;
-    arr = arr.map((memo, elem, i) =>
-        elem === index - 1 ? arr[index] : elem === index ? leftVal : memo
+    if (indexOfKey === 0) return arr;
+    arr = arr.map((elem, i) => {
+        if (i === indexOfKey - 1) {
+            return arr[indexOfKey]
+        } else if (i === indexOfKey) {
+            return leftVal
+        } else {
+            return elem;
+        }
+    }
     );
     return arr;
 }
 
 function swapRight(arr, key) {
-    const index = arr.indexOf(key);
-    const leftVal = arr[index + 1];
+    const indexOfKey = arr.indexOf(key);
+    const leftVal = arr[indexOfKey + 1];
 
-    if (index === arr.length - 1) return arr;
-    arr = arr.map((memo, elem, z) =>
-        elem === index + 1 ? arr[index] : elem === index ? leftVal : memo
+    if (indexOfKey === arr.length - 1) return arr;
+    arr = arr.map((elem, i) => {
+        if (i === indexOfKey + 1) {
+            return arr[indexOfKey]
+        } else if (i === indexOfKey) {
+            return leftVal
+        } else {
+            return elem;
+        }
+    }
     );
     return arr;
 }
 
-myArray = ['abc', 'xyz', 1, 2, 'Hey!']
 
 // call move left function with 'xyz' and myArray as arguments
 console.log(swapLeft(myArray, 'xyz'))   // ['xyz', 'abc', 1, 2, 'Hey!']
 
 // call move left function again, same arguments
 // Note that 'xyz' is already as far left as it can go
-console.log(swapLeft(myArray, 'xyz')) // ['xyz', 'abc', 1, 2, 'Hey!'] no change
+console.log(swapLeft(['xyz', 'abc', 1, 2, 'Hey!'], 'xyz')) // ['xyz', 'abc', 1, 2, 'Hey!'] no change
 
 // call move right function this time, with 2 and myArray as arguments
 console.log(swapRight(myArray, 2)) // ['xyz', 'abc', 1, 'Hey!', 2]
 
 // call move right function again, same arguments
 // Note that 2 is already as far right as it can go
-console.log(swapRight(myArray, 2)) // ['xyz', 'abc', 1, 'Hey!', 2] no change
+console.log(swapRight(['abc', 'xyz', 1, 'Hey!', 2], 2)) // ['xyz', 'abc', 1, 'Hey!', 2] no change
