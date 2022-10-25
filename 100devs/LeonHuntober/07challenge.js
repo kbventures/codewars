98f384481b446c8c0fef6d694ea4dd03ca518fd8
kbventures <kmb_x@hotmail.com>
2022-10-25 11:11:52 -0600

    8kyu find average recursion version

---
+++ 100devs/LeonHuntober/07challenge.js
@@ -0,0 +1,12 @@
+//Return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+//For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+//Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+function solve(arr) { }
+
+solve([[1, 2], [4], [5, 6]]), 4)
+solve([[1, 2], [4, 4], [5, 6, 6]]), 4)
+solve([[1, 2], [3, 4], [5, 6]]), 8)
+solve([[1, 2, 3], [3, 4, 6, 6, 7], [8, 9, 10, 12, 5, 6]]), 72)
