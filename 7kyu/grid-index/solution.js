ad8b4f9d39466d63a6091dda8f88d1873cab7aa8
kbventures <kmb_x@hotmail.com>
2022-10-31 15:24:51 -0600

    7kyu Grid Index

---
+++ 7kyu/grid-index/solution.js
@@ -0,0 +1,25 @@
+// Brute Force
+
+function gridIndex(grid, indices) {
+    let result = ""
+    indices.forEach(elem => {
+        let current = 1;
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+
+                if (elem == current) {
+                    result += grid[i][j]
+                }
+                current++
+            }
+        }
+    })
+    return result;
+}
+
+
+// Codewars Solutions
+const gridIndex = (grid, indices) => {
+    const flattened = grid.flat();
+    return indices.map(i => flattened[i - 1]).join('');
+}
