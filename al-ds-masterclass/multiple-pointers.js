1b6762994ddc439be8514e98378ba771584e67ad
kbventures <kmb_x@hotmail.com>
2022-11-01 09:36:54 -0600

    Algorithm and Data Structures Master Class Multiple Pointers

--- al-ds-masterclass/multiple-pointers.js
+++ al-ds-masterclass/multiple-pointers.js
@@ -12,16 +12,6 @@ console.log(sumZero([-2, 0, 1, 3])) // undefined
 console.log(sumZero([1, 2, 3])) // undefined
 
 
-
-
-
-
-
-
-
-
-
-
 // Solution
 
 // function sumZero(arr) {
@@ -40,3 +30,32 @@ console.log(sumZero([1, 2, 3])) // undefined
 //         }
 //     }
 // }
+
+
+
+
+// countUniqueValues
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+
+function countUniqueValues(arr) {
+    if (arr.length == 0) return 0
+    let i = 0;
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++
+            arr[i] = arr[j]
+        }
+    }
+
+    return i + 1;
+} // O(n) Time
+
+function countUniqueValues(arr) {
+    return new Set(arr).size;
+}
+
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2])) // 2
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])) // 7
+console.log(countUniqueValues([])) // 0
+console.log(countUniqueValues([-2, -1, -1, 0, 1])) // 4
