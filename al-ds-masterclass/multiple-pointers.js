1e828fd646c15ccc5de5678e3e6a0736e71a302f
kbventures <kmb_x@hotmail.com>
2022-11-01 13:53:39 -0600

    6kyu String Transformer

--- al-ds-masterclass/multiple-pointers.js
+++ al-ds-masterclass/multiple-pointers.js
@@ -14,6 +14,17 @@ console.log(sumZero([1, 2, 3])) // undefined
 
 // Solution
 
+// Brute Force
+// function sumZero(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[i] + arr[j] === 0) {
+//                 return [arr[i], arr[j]];
+//             }
+//         }
+//     }
+// }
+
 // function sumZero(arr) {
 //     let left = 0;
 //     let right = arr.length - 1;
