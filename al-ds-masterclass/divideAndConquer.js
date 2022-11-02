4916535aee9f4abb2cb2b71a3d857edfafb3c215
kbventures <kmb_x@hotmail.com>
2022-11-02 10:17:37 -0600

    7kyu sum of all arrays divide and conquer sliding window of al and ds master class and huntober leons huntober challenge 11

---
+++ al-ds-masterclass/divideAndConquer.js
@@ -0,0 +1,42 @@
+// Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located. 
+// If the value is not found, return -1
+
+
+
+// Naive
+function search(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Optiomal
+function search(array, val) {
+
+    let min = 0;
+    let max = array.length - 1;
+
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = array[middle];
+
+        if (array[middle] < val) {
+            min = middle + 1;
+        }
+        else if (array[middle] > val) {
+            max = middle - 1;
+        }
+        else {
+            return middle;
+        }
+    }
+
+    return -1;
+}
+
+search([1, 2, 3, 4, 5, 6], 4) // 3
+search([1, 2, 3, 4, 5, 6], 6) // 5
+search([1, 2, 3, 4, 5, 6], 11) // -1
