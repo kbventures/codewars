3f5b1682d2a6280f28d57181442f2b399626d82d
kbventures <kmb_x@hotmail.com>
2022-11-23 16:49:08 -0600

    7kyu training 24-25-30 al&ds binary search

---
+++ 7kyu/training-30-reduce/solution.js
@@ -0,0 +1,11 @@
+function tailAndHead(arr) {
+    var prod = 1;
+    arr.reduce((prev, curr) => {
+        var s1 = prev.toString();
+        var s2 = curr.toString();
+        prod *= parseInt(s1[s1.length - 1]) + parseInt(s2[0]);
+        return curr
+    });
+    return prod;
+}
+
