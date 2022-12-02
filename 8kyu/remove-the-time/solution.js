6ea208afd2f0bcd7935359e2d3b32e7c999f0308
kbventures <kmb_x@hotmail.com>
2022-12-01 19:13:17 -0600

    7kyu absent vowel, 8kyu parse float remove the time and updated bubblesort code form al & ds mastercall

---
+++ 8kyu/remove-the-time/solution.js
@@ -0,0 +1,5 @@
+function shortenToDate(longDate) {
+    // your code here
+
+    return longDate.split(',')[0]
+}
---
+++ al-ds-masterclass/bubblesort-optimised.js
@@ -0,0 +1,97 @@
+/*
+BubbleSort Pseudocode
+Let's visualize this!
+
+Start looping from with a variable called i the end of the array towards the beginning
+Start an inner loop with a variable called j from the beginning until i - 1
+If arr[j] is greater than arr[j+1], swap those two values!
+Return the sorted array
+*/
+
+
+var arrayRandom = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+var arrayOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var arrayReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+
+function bubbleSort(array) {
+    var swapped;
+    do {
+        swapped = false;
+        for (var i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(array, i, i + 1);
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return array;
+}
+
+console.log(bubbleSort(arrayRandom))
+
+
+
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+function bubbleSort(arr) {
+    for (var i = arr.length; i > 0; i--) {
+        for (var j = 0; j < i - 1; j++) {
+            console.log(arr, arr[j], arr[j + 1]);
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// ES2015 Version
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    return arr;
+}
+
+bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
+
+
+
+// Optimized BubbleSort with noSwaps
+function bubbleSort(arr) {
+    var noSwaps;
+    for (var i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for (var j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) break;
+    }
+    return arr;
+}
+
+bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
+
+
+
+
