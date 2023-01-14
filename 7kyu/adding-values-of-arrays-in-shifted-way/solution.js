b2ca4b5cb05bf905af9921c361a80e5279d09a2f
kbventures <kmb_x@hotmail.com>
2023-01-14 10:55:29 -0600

    7kyu how many arguments

--- 7kyu/adding-values-of-arrays-in-shifted-way/solution.js
+++ 7kyu/adding-values-of-arrays-in-shifted-way/solution.js
@@ -4,12 +4,35 @@ function addingShifted(arrayOfArrays, shift) {
     for (var i = 0; i < endArrayLength; i++) {
         result.push(0);
     }
-
     for (var i = 0; i < arrayOfArrays.length; i++) {
         for (var j = 0; j < arrayOfArrays[i].length; j++) {
             result[j + shift * i] += arrayOfArrays[i][j];
         }
     }
+    return result;
+}
 
+
+// Alternative Solution: Brute Force
+function addingShifted(arrayOfArrays, shiftAmount) {
+    let currentShift = 0;
+    let result = [];
+    // iterate through array of arrays
+    for (let i = 0; i < arrayOfArrays.length; i++) {
+        let array = arrayOfArrays[i];
+        // iterate through array values
+        for (let j = 0; j < array.length; j++) {
+            let arrayValue = array[j]
+            // if we dont have something at shifted index in result array, push it
+            if (result[j + currentShift] === undefined) {
+                result.push(arrayValue)
+            } else {
+                // otherwise add array value
+                result[j + currentShift] += arrayValue
+            }
+        }
+        // update shift for next array
+        currentShift += shiftAmount
+    }
     return result;
 }
