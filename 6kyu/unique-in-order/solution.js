685b52af7f13f41995d04aef344cf73661f63605
kbventures <kmb_x@hotmail.com>
2023-01-17 14:27:15 -0600

    6kyu unique in order

---
+++ 6kyu/unique-in-order/solution.js
@@ -0,0 +1,10 @@
+var uniqueInOrder = function (iterable) {
+    //your code here - remember iterable can be a string or an array
+    let result = [];
+    for (let i = 0; i < iterable.length; i++) {
+        if (iterable[i] !== iterable[i + 1]) {
+            result.push(iterable[i]);
+        }
+    }
+    return result;
+}
