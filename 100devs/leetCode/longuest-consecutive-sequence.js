34a828fed63eb1753598c35b25366a3abae24088
kbventures <kmb_x@hotmail.com>
2023-02-04 10:13:29 -0600

    Leetcode Longuest Consecutive sequence optimised

--- 100devs/leetCode/longuest-consecutive-sequence.js
+++ 100devs/leetCode/longuest-consecutive-sequence.js
@@ -24,15 +24,30 @@ var longestConsecutive = function (t) {
 
 };
 
+// Solution
+var longestConsecutive = function (nums) {
+    // Handle base case of empty array
+    if (!nums.length) return 0;
 
-/*
-1.Create an empty hash
-2.Insert all array elements to hash
-3.Do following for every element arr[i]
-4. Check if this element is the starting point of a subsequence. 
-To check this, simply look for arr[i]-1 in the hash, if not found
-, then this is the first element of a subsequence.
-5. If this element is the first element, then count the number of elements in the consecutive starting with
-this element. Iterate from arr[i] +1 till the last element can be found. 
-6. If the coun tis more than the previosu longuest subsequence found, then upate this. 
-*/
+    // Give ability to look up nums by value
+    const set = new Set(nums);
+    let max = 0;
+
+    for (const num of set) {
+        // Make sure we are starting at the beginning of the sequenece
+        if (set.has(num - 1)) continue;
+
+        let currNum = num;
+        let currMax = 1;
+
+        // Look numbers that make a consecutive sequence
+        while (set.has(currNum + 1)) {
+            currNum++;
+            currMax++;
+        }
+        // Update max
+        max = Math.max(max, currMax);
+    }
+
+    return max;
+};
