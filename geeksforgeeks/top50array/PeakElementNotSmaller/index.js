edd736ed8a0676f06ba14c5bb0518f6a927322cf
kbventures <kmb_x@hotmail.com>
2024-02-21 13:35:48 -0500

    Top 50 Array Geeks For Geeks Peak Element Not Smaller

--- geeksforgeeks/top50array/PeakElementNotSmaller/index.js
+++ geeksforgeeks/top50array/PeakElementNotSmaller/index.js
@@ -26,28 +26,64 @@
 
 
 
+// // It can go from increasing to decrasing more than once but always in this order
+// // There can be more tha one valid peak
+// function peakEle(arr){
+// let result = []
+// // console.log(arr[0],arr[1], arr[arr.length-1], arr[arr.length-2])
+// if(arr[0]> arr[1]){
+//     result.push(arr[0])
+// } 
+// // console.log(arr[arr.length-1], arr[arr.length-2])
+// if(arr[arr.length-1] > arr[arr.length-2]){
+//     result.push(arr[arr.length-1])
+// }
+
+// if(arr.length < 3) return result
+
+// for(let i = 1; i < arr.length -1;i++){
+//     if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+//         result.push(arr[i])
+//     }
+// }
+// return result;
+// }
+
+// // Returns 20 and 90
+// // const result = peakEle([10, 20, 15, 2, 23, 90, 67])
+// const result1 = peakEle([20, 10, 20, 10,20])
+// console.log(result1)
+
+
 // It can go from increasing to decrasing more than once but always in this order
 // There can be more tha one valid peak
 function peakEle(arr){
-let result = []
-if(arr[0]< arr[1]){
-    arr.push(arr[1])
-} 
-if(arr[arr.length-1] > arr[arr.length-2]){
-    arr.push(arr[arr.length-1])
-}
-
-if(arr.length < 3) return result
-
-for(let i = 1; i < arr.length -2;i++){
-    if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
-        result.push(arr[i])
+    if(arr.length <=1) return arr
+    let result = [];
+    for(let i = 0; i <= arr.length -1;i++){
+        if(arr[i-1] == undefined){
+            if(arr[i] > arr[i+1]){
+                result.push(arr[i])
+            }
+        } else if (arr[i+1] == undefined){
+            if(arr[i] > arr[i-1]){
+                result.push(arr[i])
+            }
+        } else {
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                result.push(arr[i])
+            }
+        }
     }
-}
-return result;
-}
 
-// Returns 20 and 90
-const result = peakEle([10, 20, 15, 2, 23, 90, 67])
+    if(result.length == 0) result.push(arr[0])
+    return result;
+    }
+    
+    // Returns 20 and 90
+    // const result = peakEle([10, 20, 15, 2, 23, 90, 67])
+    const result1 = peakEle([20, 20,20,20])
+    console.log(result1)
+
 
 
