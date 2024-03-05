9d6dffd19a55b7824da1085b5b7a0ce19dd0fd54
kbventures <kmb_x@hotmail.com>
2024-03-05 10:13:47 -0500

    Geeks For Geeks Top 50 hash map challenges Find first non repeating character

--- geeksforgeeks/HashMap/FindFirstNonRepChar/index.js
+++ geeksforgeeks/HashMap/FindFirstNonRepChar/index.js
@@ -41,4 +41,53 @@
 // console.log(result)
 
 
-// V2 Hashmap the whole world first and keep track of the first occurence of a single letter while iterating over it!
+// V2 Hashmap the whole world first and keep track of the first occurence of a single letter while iterating over it!(My version!)
+
+
+// function FindFirstNonRepChar(arr){
+// let HashMap = {}
+
+// for(const i of arr){
+//     if(!HashMap[i]){
+//         HashMap[i]=1; 
+//     } else {
+//         HashMap[i]++
+//     }
+// }
+
+// for(const z in HashMap){
+//     if(HashMap[z] == 1) return z
+// }
+
+// }
+
+// const result  = FindFirstNonRepChar("Geeksforgeeks")
+// console.log(result)
+
+
+// V3 Improved version but not mine 
+function findFirstNonRepChar(str) {
+    const charCountMap = {};
+    const charOrder = [];
+
+    for (const char of str) {
+        charCountMap[char] = (charCountMap[char] || 0) + 1;
+
+        // Keep track of the order of characters encountered
+        if (charCountMap[char] === 1) {
+            charOrder.push(char);
+        } else {
+            // Remove from the order if the character is repeating
+            const index = charOrder.indexOf(char);
+            if (index !== -1) {
+                charOrder.splice(index, 1);
+            }
+        }
+    }
+
+    // The first element in charOrder is the first non-repeating character
+    return charOrder.length > 0 ? charOrder[0] : null;
+}
+
+const result = findFirstNonRepChar("Geeksforgeeks");
+console.log(result);
