16fd1fad38e2e5761366eddd2d5f1f48f1ff34f9
kbventures <kmb_x@hotmail.com>
2024-03-08 10:32:31 -0500

    Geeks for geeks top 50 hash map challenges TwoSum using two pointers

--- geeksforgeeks/HashMap/TwoSum/index.js
+++ geeksforgeeks/HashMap/TwoSum/index.js
@@ -22,24 +22,68 @@
 // V1 Naive version: Nested Traversal
 // T O(n^2) S(1)
 
-function TwoSum(arr,sum){
+// function TwoSum(arr,sum){
 
-    // Looking for true
-    for(let i = 0; i< arr.length -1;i++){
-        for(let y = i+1; y<= arr.length-1;y++){
-            if((arr[i]+arr[y]) == sum) return true; 
-        }
-    }
+//     // Looking for true
+//     for(let i = 0; i< arr.length -1;i++){
+//         for(let y = i+1; y<= arr.length-1;y++){
+//             if((arr[i]+arr[y]) == sum) return true; 
+//         }
+//     }
     
-    // False by default 
-    return false;
-}
+//     // False by default 
+//     return false;
+// }
 
-const result = TwoSum([0, -1, 2, -3, 1], -2)
+// const result = TwoSum([0, -1, 2, -3, 1], -2)
 
-// Is true?
-console.log(result)
+// // Is true?
+// console.log(result)
 
 // V2 HashSet
 
+// function TwoSum(arr,sum){
+
+// let HashMap={}
+//     for(const i of arr){
+//         if(!HashMap[i]){
+//             HashMap[i] = i;
+//         }
+//     }
+//     console.log(HashMap)
+
+//     // False by default 
+//     return false;
+// }
+
+// const result = TwoSum([0, -1, 2, -3, 1], -2)
+
+// // Is true?
+// console.log(result)
+
 // V3 Two Pointers
+
+
+function TwoSum(arr,sum){
+let start =0;
+let end = 1
+
+while(start< arr.length){
+    let temp = arr[start] + arr[end];
+    if(temp == sum) {
+        return true
+    } else if (end != arr.length -1) {
+        end++
+    } else {
+        start++
+        end = start + 1
+    }
+    }
+        // False by default 
+        return false;
+    }
+    
+    const result = TwoSum([0, -1, 2, -3, 1], -2)
+    
+    // Is true?
+    console.log(result)
