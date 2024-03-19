40f82f99c8ab1e83e87b06567a7fff1a986c7b4f
kbventures <kmb_x@hotmail.com>
2024-03-19 09:50:53 -0500

    Two Pointers Leedcode Reverse Vowels

--- geeksforgeeks/TwoPointers/RunningFromBothEndsArray/ReverseVowels/index.js
+++ geeksforgeeks/TwoPointers/RunningFromBothEndsArray/ReverseVowels/index.js
@@ -20,28 +20,59 @@
 var reverseVowels = function(s) {
 
     // Only reverse vowels for each other
-
+    let stringArr = s.split("");
     let start = 0;
-    let end = s.length -1
+    let end = stringArr.length -1;
+    let endIndex = stringArr.length -1; 
+    // console.log(Math.floor((endIndex+2)/2))
     let vowels = ['a','e','i','o','u']
+    // console.log(Math.floor((endIndex+2)/2))
     // Math.floor((end+1)/2) Uneven middile 5 = 3 Even middle 4 = 3
-for (let i = 0; i < Math.floor((end + 1) / 2); i++) {
-    console.log(s[start], s[end])
-    if(vowels.includes(s[start]) && vowels.includes(s[end])){
-        let temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
+for (let i = 0; i <= Math.floor((endIndex + 1) / 2); i++) {
+    console.log(stringArr[start], stringArr[end])
+    if(vowels.includes(stringArr[start].toLowerCase()) && vowels.includes(stringArr[end].toLowerCase())){
+        console.log("working?")
+        let temp = stringArr[start];
+        stringArr[start] = stringArr[end];
+        stringArr[end] = temp;
         start++;
         end--;
-    } else if (vowels.includes(s[start])){
+        // console.log(stringArr)
+    } else if (!vowels.includes(stringArr[start].toLowerCase())){
+        console.log("working 2")
         start++
-    } else if(vowels.includes(s[end])){
+        if(!vowels.includes(stringArr[end].toLowerCase())){
+            console.log("working 2.1")
+            end--
+        }
+    } else if (!vowels.includes(stringArr[end].toLowerCase())){
+        console.log("working 3")
         end--
     }
-
+    if(stringArr.length == 2) return stringArr.join("")
 }
 
-console.log(s)    
+return stringArr.join("") 
 };
 
-reverseVowels("hello")
+// const result = reverseVowels("hello")
+// // const result = reverseVowels("leetcode")
+// console.log("result",result)
+// // // Output: "leotcede"
+// console.log("result",result)
+
+// const result = reverseVowels("Ai")
+// // // Output: "leotcede"
+// console.log("result",result)
+
+// const result = reverseVowels("race a car")
+// console.log("result",result)
+// // expected "raca e car"
+
+
+
+const result = reverseVowels("Marge, let's \"went.\" I await news telegram.")
+// const result = reverseVowels("leetcode")
+console.log("result",result)
+
+// Expected Marge, let's \"went.\" i awaIt news telegram.
