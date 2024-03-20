0c676966f1e1db85f7a234b906d6062fe01a49fe
kbventures <kmb_x@hotmail.com>
2024-03-20 09:54:04 -0500

    Leetcode reverse vowels two pointer challenge

--- geeksforgeeks/TwoPointers/RunningFromBothEndsArray/ReverseVowels/index.js
+++ geeksforgeeks/TwoPointers/RunningFromBothEndsArray/ReverseVowels/index.js
@@ -17,62 +17,79 @@
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function(s) {
 
-    // Only reverse vowels for each other
+// V1 not working
+// var reverseVowels = function(s) {
+//     let stringArr = s.split("");
+//     let start = 0;
+//     let end = stringArr.length -1;
+//     let endIndex = stringArr.length -1; 
+//     let vowels = ['a','e','i','o','u']
+// for (let i = 0; i <= Math.floor((endIndex + 1) / 2); i++) {
+//     console.log(stringArr[start],stringArr[end])
+//     if(vowels.includes(stringArr[start].toLowerCase()) && vowels.includes(stringArr[end].toLowerCase())){
+//         // console.log(stringArr[start],stringArr[end]);
+//         let temp = stringArr[start];
+//         stringArr[start] = stringArr[end];
+//         stringArr[end] = temp;
+//         start++;
+//         end--;
+//         // console.log(stringArr)
+//     } else if (!vowels.includes(stringArr[start].toLowerCase())){
+//         start++
+//         if(!vowels.includes(stringArr[end].toLowerCase())){
+//             end--
+//         }
+//     } else if (!vowels.includes(stringArr[end].toLowerCase())){
+//         end--
+//     }
+//     if(stringArr.length == 2) return stringArr.join("")
+// }
+
+// return stringArr.join("") 
+// };
+
+
+
+// // Expected Marge, let's \"went.\" i awaIt news telegram.
+// // Outpust  Marge, let's "went." I await news telegram.
+
+// const result = reverseVowels("Marge, let's \"went.\" I await news telegram.")
+// // const result = reverseVowels("leetcode")
+// console.log("result",result)
+
+
+//V2
+
+var reverseVowels = function(s) {
     let stringArr = s.split("");
     let start = 0;
     let end = stringArr.length -1;
-    let endIndex = stringArr.length -1; 
-    // console.log(Math.floor((endIndex+2)/2))
-    let vowels = ['a','e','i','o','u']
-    // console.log(Math.floor((endIndex+2)/2))
-    // Math.floor((end+1)/2) Uneven middile 5 = 3 Even middle 4 = 3
-for (let i = 0; i <= Math.floor((endIndex + 1) / 2); i++) {
-    console.log(stringArr[start], stringArr[end])
-    if(vowels.includes(stringArr[start].toLowerCase()) && vowels.includes(stringArr[end].toLowerCase())){
-        console.log("working?")
-        let temp = stringArr[start];
-        stringArr[start] = stringArr[end];
-        stringArr[end] = temp;
-        start++;
-        end--;
-        // console.log(stringArr)
-    } else if (!vowels.includes(stringArr[start].toLowerCase())){
-        console.log("working 2")
-        start++
-        if(!vowels.includes(stringArr[end].toLowerCase())){
-            console.log("working 2.1")
+    let vowels = ['a','e','i','o','u','A','E','I','O','U']
+    
+    while( start < end){
+
+        if(vowels.includes(stringArr[start]) && vowels.includes(stringArr[end])){
+            let temp = stringArr[start];
+            stringArr[start]=stringArr[end];
+            stringArr[end]= temp;
+            start++
+            end--
+        } else if (!vowels.includes(stringArr[start])){
+            start++
+        } else if (!vowels.includes(stringArr[end])){
             end--
         }
-    } else if (!vowels.includes(stringArr[end].toLowerCase())){
-        console.log("working 3")
-        end--
-    }
-    if(stringArr.length == 2) return stringArr.join("")
-}
 
-return stringArr.join("") 
+    }
+    return stringArr.join('')
 };
 
-// const result = reverseVowels("hello")
-// // const result = reverseVowels("leetcode")
-// console.log("result",result)
-// // // Output: "leotcede"
-// console.log("result",result)
-
-// const result = reverseVowels("Ai")
-// // // Output: "leotcede"
-// console.log("result",result)
-
-// const result = reverseVowels("race a car")
-// console.log("result",result)
-// // expected "raca e car"
 
 
+// Expected Marge, let's \"went.\" i awaIt news telegram.
+// Outpust  Marge, let's "went." I await news telegram.
 
 const result = reverseVowels("Marge, let's \"went.\" I await news telegram.")
 // const result = reverseVowels("leetcode")
 console.log("result",result)
-
-// Expected Marge, let's \"went.\" i awaIt news telegram.
