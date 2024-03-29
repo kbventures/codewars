a815fd4e785885bd75013f9713ec81a23b9080bd
kbventures <kmb_x@hotmail.com>
2024-03-29 11:35:56 -0500

    Leetcode Palindrome 2 Two Pointer Version

--- geeksforgeeks/TwoPointers/RunningFromBothEndsArray/ValidPalindrome2/index.js
+++ geeksforgeeks/TwoPointers/RunningFromBothEndsArray/ValidPalindrome2/index.js
@@ -23,58 +23,133 @@
 // s consists of lowercase English letters.
 
 // V1 Brute Force
-var validPalindrome = function(s) {
+// var validPalindrome = function(s) {
 
-    let sArr = s.split("")
-    let start =0;
-    let end = s.length -1; 
+//     let sArr = s.split("")
+//     let start =0;
+//     let end = s.length -1; 
 
-    while(sArr[start] == sArr[end]){
-        // console.log(sArr[start],sArr[end])
-        start++
-        end--
-        if( start == s.length-1 )return true; 
+//     while(sArr[start] == sArr[end]){
+//         // console.log(sArr[start],sArr[end])
+//         start++
+//         end--
+//         if( start == s.length-1 )return true; 
 
-    }
-    const leftArr = sArr.slice(0,start).concat(sArr.slice(start+1))
-    const rightArr = sArr.slice(0, end).concat(sArr.slice(end + 1))
-    start = 0;
-    end = rightArr.length -1;
+//     }
+//     const leftArr = sArr.slice(0,start).concat(sArr.slice(start+1))
+//     const rightArr = sArr.slice(0, end).concat(sArr.slice(end + 1))
+//     start = 0;
+//     end = rightArr.length -1;
 
-    while(rightArr[start] == rightArr[end]){
-        start++
-        end--
-        if( start == s.length-1 )return true; 
+//     while(rightArr[start] == rightArr[end]){
+//         start++
+//         end--
+//         if( start == s.length-1 )return true; 
 
-    }
+//     }
 
-    start = 0;
-    end = leftArr.length -1; 
+//     start = 0;
+//     end = leftArr.length -1; 
 
-    while(leftArr[start] == leftArr[end]){
-        // console.log(sArr[start],sArr[end])
-        start++
-        end--
-        if( start == s.length-1 )return true; 
+//     while(leftArr[start] == leftArr[end]){
+//         // console.log(sArr[start],sArr[end])
+//         start++
+//         end--
+//         if( start == s.length-1 )return true; 
 
-    }
-
-
+//     }
 
 
 
-    return false; 
-};
-
-const result = validPalindrome("abcdefghhgfedcwba")
-console.log(result)
 
 
+//     return false; 
+// };
+
+// const result = validPalindrome("abcdefghhgfedcwba")
+// console.log(result)
+
+
+// V2 Two Pointers
 
 /**
  * @param {string} s
  * @return {boolean}
  */
+
+
+// var validPalindrome = function(s) {
+
+//     let sArr = s.split("")
+//     let start =0;
+//     let end = s.length -1; 
+
+//     while(sArr[start] == sArr[end]){
+//         start++
+//         end--
+//         if( start >= end)return true; 
+
+//     }
+
+//     let startBreak = start;
+//     let endBreak = end;
+
+//     // Left Check
+//     while(sArr[start + 1] == sArr[end]){
+//         start++
+//         end--
+//         if( start >= end)return true; 
+//     }
+//     // Right Check 
+//     while(sArr[startBreak] == sArr[endBreak-1]){
+//         startBreak++
+//         endBreak--
+//         if( startBreak >= endBreak)return true; 
+//     }
+
+//     return false;
+// };
+
+// const result = validPalindrome("cbbcc")
+// console.log(result)
+
+// V2 Two pointer Refactor
+
+var validPalindrome = function(s) {
+
+    let start =0;
+    let end = s.length -1; 
+
+    while(s[start] == s[end]){
+        start++
+        end--
+        if( start >= end)return true; 
+
+    }
+
+    let startBreak = start;
+    let endBreak = end;
+
+    // Left Check
+    while(s[start + 1] == s[end]){
+        start++
+        end--
+        if( start >= end)return true; 
+    }
+    // Right Check 
+    while(s[startBreak] == s[endBreak-1]){
+        startBreak++
+        endBreak--
+        if( startBreak >= endBreak)return true; 
+    }
+
+    return false;
+};
+
+const result = validPalindrome("cbbcc")
+console.log(result)
+
+
 
 
 
