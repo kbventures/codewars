e9f76514fa208b7564d295f16f93663026ebd55d
kbventures <kmb_x@hotmail.com>
2024-04-01 12:28:54 -0500

    Leecode Sort Array By Parity V1 Two Pointers

--- geeksforgeeks/TwoPointers/RunningFromBothEndsArray/SortArrByParity/index.js
+++ geeksforgeeks/TwoPointers/RunningFromBothEndsArray/SortArrByParity/index.js
@@ -22,3 +22,56 @@
 
 // 1 <= nums.length <= 5000
 // 0 <= nums[i] <= 5000
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+// V1 Twoter pointers
+// var sortArrayByParity = function(nums) {
+//     let index1 = 0;
+//     let index2 = 0;
+
+//     while(index1 < nums.length){
+//         if(nums[index1] % 2 === 1){
+//             index1++
+//         } else {
+//             let temp = nums[index2];
+//             nums[index2]= nums[index1];
+//             nums[index1] = temp
+//             index1++
+//             index2++
+//         }
+//     }
+//     return nums
+// };
+
+// // Output
+// // [3,1,2,4]
+// // Expected
+// // [2,4,3,1]
+
+// const result0 = sortArrayByParity([3,1,2,4])
+// console.log(result0)
+
+
+// V2
+var sortArrayByParity = function(nums) {
+    let left = 0;
+    let right = nums.length -1;
+
+    while(left < right){
+       
+    }
+    return nums
+};
+
+// Output
+// [3,1,2,4]
+// Expected
+// [2,4,3,1]
+
+const result0 = sortArrayByParity([3,1,2,4])
+console.log(result0)
