7d72b17860adbad4382f894a16449246a49d739a
kbventures <kmb_x@hotmail.com>
2024-04-22 14:59:18 -0500

    Geeks for geeks Stack Top 50 reverseString Array Version

--- geeksforgeeks/Stacks/reverseString/index.js
+++ geeksforgeeks/Stacks/reverseString/index.js
@@ -24,3 +24,69 @@
 //     One by one pop all characters from stack and put them back to string.
 
 // Below is the implementation of the above approach:
+
+class Stack {
+    constructor(){
+        this.items = []
+    }
+
+    // Push method adds a new element to the top of the stack
+    push(element){
+        this.items.push(element);
+    }
+
+    // Pop method removes the top element from the stack and returns it
+    pop(){
+        if(this.isEmpty()){
+            return "Underflow"
+        }
+        return this.items.pop()
+    }
+
+    // Peek method returns the top element of the stack without removing
+    peek(){
+        return this.items[this.items.length -1]
+    }
+
+    // isEmpty method checks if the stack is empty
+    isEmpty(){
+        return this.items.length === 0;
+    }
+
+    // size method removes all elements from the stack
+    clear(){
+        this.items = [];
+    }
+
+    // print method displays the elements of the stack
+    print(){
+        console.log(this.items.toString())
+    }
+    lth(){
+        return this.items.length;
+    }
+}
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    const currStack = new Stack()
+    let result = "";
+
+    for(let i=0;i<s.length;i++){
+        console.log(s[i])
+        currStack.push(s[i])
+    }
+    currStack.print()
+    const stackLength = currStack.lth()
+    for(let i =0; i<stackLength;i++){
+        result += currStack.pop()
+    }
+    return result;
+};
+
+const result0 = reverseString('hello')
+
+console.log(result0)
