14aa0127c796541c97a860f10470598b21054b61
kbventures <kmb_x@hotmail.com>
2024-05-15 13:13:15 -0500

    Stack Queue Challenges

---
+++ geeksforgeeks/Stacks/index.js
@@ -0,0 +1,13 @@
+// https://www.geeksforgeeks.org/delete-middle-element-stack/
+
+
+// Delete middle element of a stack
+// Last Updated : 10 Feb, 2023
+
+// Given a stack with push(), pop(), and empty() operations, The task is to delete the middle element of it without using any additional data structure.
+
+//     Input  : Stack[] = [1, 2, 3, 4, 5]
+//     Output : Stack[] = [1, 2, 4, 5]
+
+//     Input  : Stack[] = [1, 2, 3, 4, 5, 6]
+//     Output : Stack[] = [1, 2, 4, 5, 6]
