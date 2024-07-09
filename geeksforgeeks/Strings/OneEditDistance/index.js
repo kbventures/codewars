0e1b2f5b8b7cad3727f46a3e4b18a9e9e6e47c78
kbventures <kmb_x@hotmail.com>
2024-07-09 09:17:28 -0500

    Leetcode one edit distance 2 pointer and recursion optimal versions

--- geeksforgeeks/Strings/OneEditDistance/index.js
+++ geeksforgeeks/Strings/OneEditDistance/index.js
@@ -21,60 +21,143 @@
 // Output: false
 // Explanation: We cannot get t from s by only one step.
 
-var oneEditDistance = function(s,t) {
-let diff  = Math.abs(s.length - t.length)
 
-// If more than one character difference one insert not possible
-if(diff > 1) return false; 
+// // V1 Brute force
+// var oneEditDistance = function(s,t) {
+// let diff  = Math.abs(s.length - t.length)
 
-let editCount = 0; 
+// // If more than one character difference one insert not possible
+// if(diff > 1) return false; 
 
-// Replace a character of s to get t
-if(s.length == t.length){
-    for(let i in t){
-        if(s[i] !== t[i]){
-            let temp1 = s.split('')
-            temp1[i] = t[i]
-            let temp2 = temp1.join('')
-            if(temp2 === t){
-                editCount++
-                if(editCount ==2) return false; 
-            } 
-        }
-    }
-} 
-// Insert a character into s to get t
-if((s.length+1)== t.length){
-    for(let y =0; y<t.length;y++){
-        let temp1 = s.slice(0,y) + t[y] + s.slice(y); 
-        console.log("temp1",temp1)
-        if(temp1 == t){
-            editCount++
-                if(editCount ==2) return false; 
-        }
-    }
-}
+// let editCount = 0; 
 
-// Delete a character from s to get t
-if((s.length-1)== t.length){
-    for (let i = 0; i < s.length; i++) {
-        let newStr = s.slice(0, i) + s.slice(i + 1);
-        if(newStr == t){
-            editCount++
-                if(editCount ==2) return false; 
-        }
-    }
-}
+// // Replace a character of s to get t
+// if(s.length == t.length){
+//     for(let i in t){
+//         if(s[i] !== t[i]){
+//             let temp1 = s.split('')
+//             temp1[i] = t[i]
+//             let temp2 = temp1.join('')
+//             if(temp2 === t){
+//                 editCount++
+//                 if(editCount ==2) return false; 
+//             } 
+//         }
+//     }
+// } 
+// // Insert a character into s to get t
+// if((s.length+1)== t.length){
+//     for(let y =0; y<t.length;y++){
+//         let temp1 = s.slice(0,y) + t[y] + s.slice(y); 
+//         if(temp1 == t){
+//             editCount++
+//                 if(editCount ==2) return false; 
+//         }
+//     }
+// }
 
-    return editCount > 1 
+// // Delete a character from s to get t
+// if((s.length-1)== t.length){
+//     for (let i = 0; i < s.length; i++) {
+//         let newStr = s.slice(0, i) + s.slice(i + 1);
+//         if(newStr == t){
+//             editCount++
+//                 if(editCount ==2) return false; 
+//         }
+//     }
+// }
+
+//     return editCount > 1 
         
-}
+// }
 
-// // Delete Example
-// const result1 = oneEditDistance("abit","abi")
+// // // Delete Example
+// // const result1 = oneEditDistance("abit","abi")
+// // console.log(result1)
+
+
+// // Insert Example
+// const result1 = oneEditDistance("abix","abi")
 // console.log(result1)
 
+// // V2 Recursion
+// const isOneEditDistance = (s, t) => {
+//     if (s === t) return false;
+  
+//     const sLength = s.length;
+//     const tLength = t.length;
+//     const lengthDiff = Math.abs(sLength - tLength);
+  
+//     if (lengthDiff > 1) return false;
+  
+//     if (lengthDiff === 0) {
+//       let diffCount = 0;
+//       for (let i = 0; i < sLength; i++) {
+//         if (s[i] !== t[i]) {
+//           if (diffCount > 0) return false;
+//           diffCount++;
+//         }
+//       }
+//       return true;
+//     }
+  
+//     if (sLength > tLength) return isOneEditDistance(t, s);
+  
+//     let sx = 0,
+//       tx = 0;
+//     let seenDiff = false;
+  
+//     while (tx < tLength) {
+//       if (s[sx] !== t[tx]) {
+//         if (seenDiff) return false;
+//         seenDiff = true;
+//         tx++;
+//       } else {
+//         sx++;
+//         tx++;
+//       }
+//     }
+//     return true;
+//   };
+  
+  // Time Complexity: O(n)
+  // Space Complexity: O(1)
 
-// Insert Example
-const result1 = oneEditDistance("abix","abi")
-console.log(result1)
+  // V3
+  const isOneEditDistance = (s, t) => {
+    if (s === t) return false;
+  
+    const sLength = s.length;
+    const tLength = t.length;
+  
+    if (Math.abs(sLength - tLength) > 1) return false;
+  
+    let sx = 0,
+      tx = 0,
+      diff = 0;
+  
+    while (sx < sLength && tx < tLength) {
+      if (s[sx] === t[tx]) {
+        sx++;
+        tx++;
+        continue;
+      }
+  
+      if (diff == 0) diff++;
+      else return false;
+  
+      if (sLength === tLength) {
+        sx++;
+        tx++;
+      } else if (sLength > tLength) {
+        sx++;
+      } else {
+        tx++;
+      }
+    }
+  
+    return true;
+  };
+  
+  // Time Complexity: O(n)
+  // Space Complexity: O(1)
