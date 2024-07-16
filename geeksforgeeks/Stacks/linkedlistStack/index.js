42f00921b58f44035c170fa394618c582eefac25
kbventures <kmb_x@hotmail.com>
2024-07-16 08:53:14 -0500

    Leetcode Min Stack

--- geeksforgeeks/Stacks/linkedlistStack/index.js
+++ geeksforgeeks/Stacks/linkedlistStack/index.js
@@ -1,48 +1,59 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-}
+// class Node {
+//     constructor(data) {
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
 
-class LinkedListStack {
-    constructor() {
-        this.top = null;
-        this.size = 0;
-    }
+// class LinkedListStack {
+//     constructor() {
+//         this.top = null;
+//         this.size = 0;
+//     }
 
-    push(element) {
-        const newNode = new Node(element);
-        newNode.next = this.top;
-        this.top = newNode;
-        this.size++;
-    }
+//     push(element) {
+//         const newNode = new Node(element);
+//         newNode.next = this.top;
+//         this.top = newNode;
+//         this.size++;
+//     }
 
-    pop() {
-        if (this.isEmpty()) {
-            return undefined;
-        }
-        const removedNode = this.top;
-        this.top = this.top.next;
-        this.size--;
-        return removedNode.data;
-    }
+//     pop() {
+//         if (this.isEmpty()) {
+//             return undefined;
+//         }
+//         const removedNode = this.top;
+//         this.top = this.top.next;
+//         this.size--;
+//         return removedNode.data;
+//     }
 
-    peek() {
-        return this.isEmpty() ? undefined : this.top.data;
-    }
+//     peek() {
+//         return this.isEmpty() ? undefined : this.top.data;
+//     }
 
-    isEmpty() {
-        return this.size === 0;
-    }
+//     isEmpty() {
+//         return this.size === 0;
+//     }
 
-    getSize() {
-        return this.size;
-    }
-}
+//     getSize() {
+//         return this.size;
+//     }
+// }
 
 
 
+
+
+
+
+
+
+
+
+
+
+// Linked List Stack Review Attemp
 
 
 
