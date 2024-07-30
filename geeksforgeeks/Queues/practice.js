95da1450e2a3780e9ef8bbc2b3a31034b666aeaa
kbventures <kmb_x@hotmail.com>
2024-07-30 07:15:43 -0500

    Leetcode Valid Parentheses Concise Version

--- geeksforgeeks/Queues/practice.js
+++ geeksforgeeks/Queues/practice.js
@@ -1,10 +1,58 @@
+// V0 Array Queue
+
+// class Queue{
+// }
+
+// let tempQ = new Queue()
+
+
 // V1 Array Queue Class
-class Queue {
-    constructor(){
-        this.arr = arr; 
+
+class Node {
+    constructor(data){
+   
     }
 }
 
+class Queue {
+    constructor(){
+      
+    }
+
+    enqueue(val){
+ 
+    }
+
+    dequeue(){
+   
+    }
+    
+    peek(){
+ 
+
+    }
+    isEmpty(){
+       
+    }
+
+    length(){
+ 
+    }
+}
+
+
+
+let tempQ = new Queue()
+// tempQ.enqueue(2)
+// console.log(tempQ)
+// tempQ.enqueue(0)
+// console.log(tempQ)
+// tempQ.enqueue(3)
+// console.log(tempQ)
+// tempQ.dequeue()
+// console.log(tempQ)
+// tempQ.dequeue()
+// console.log(tempQ)
 
 
 // Enqueue
@@ -18,3 +66,30 @@ class Queue {
 // isEmpty
 
 // V2 Node Queue Class
+
+
+// Valid parentheses array version
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = []
+    let left = ["(", "[","{"]
+    let right = [")","]","}"]
+    let i =0; 
+    while(i < s.length){
+        
+        stack.push(s[i])
+        i++
+    }
+    i =0;
+    while(i<stack.length){
+        console.log()
+    }
+    console.log(stack)
+    return true; 
+};
+
+const result = isValid("()[]{}")
