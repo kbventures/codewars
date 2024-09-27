50c3ebb23e055916413d1daa0a1e1b52ca093925
kbventures <kmb_x@hotmail.com>
2024-09-27 11:46:25 -0500

    Leetcode String Compression V2 Draft

--- geeksforgeeks/Strings/StringCompression/index.js
+++ geeksforgeeks/Strings/StringCompression/index.js
@@ -5,54 +5,99 @@ https://leetcode.com/problems/string-compression/description/
 // The space complexity needs to be constant; therefore, we cannot use any additional arrays or strings for our computations.
 // The challenge is to edit the array in place since we aren't allowed to return a separate compressed string but instead must modify the chars array directly.
 
+// /**
+//  * @param {character[]} chars
+//  * @return {number}
+//  */
+// var compress = function(chars) {
+//     let s =""
+//     if(chars.length === 0){
+//         return chars
+//     } else if (chars.length ===1){
+//         s+=chars[0]
+//         return chars.length;
+//     }
+    
+//     const compressionMap = new Map()
+
+// // We iterate over the provided array, chars.
+// // For each unique character, we find the number of times it repeats consecutively.
+
+//     for( c of chars){
+//         if(compressionMap.has(c)){
+//             compressionMap.set(c, compressionMap.get(c) + 1)
+//         } else {
+//             compressionMap.set(c,1)
+//         }
+//     }    
+
+//     // We then overwrite the original chars array with the character followed by the number of occurrences if more than one.
+//     let j =0;
+//     for(c of compressionMap){
+//         // console.log(c)
+//      if(c[1]===1){
+//         s+= c[0]
+//      } else {
+//         s = s + c[0] + c[1]
+//      }
+//     }
+//     // console.log("current s:", s)
+
+//     for(c in s){
+//         console.log(c)
+//         chars[c] = s[c]
+//     }
+//     chars.length = s.length;
+//     console.log("final chars",chars)
+
+//     // We have to return the length of the array after the modifications.
+//     // console.log("chars result",chars)
+//     return chars.length;
+// };
+
+// // const result = compress(['a','a','b','b','c','c','c'])
+// const result = compress(["a","a","a","b","b","a","a"])
+// // ["a","3","b","2","a","2"]
+
+
+
+// console.log(result)
+
+
+
+// // Expected
+// // ["a","b","1","2"]
+
+
+
+
+// V2 Deals with reoccuring groups
 /**
  * @param {character[]} chars
  * @return {number}
  */
 var compress = function(chars) {
-    let s =""
-    if(chars.length === 0){
-        return chars
-    } else if (chars.length ===1){
-        s+=chars[0]
-        return chars.length;
-    }
+
+    // Check for one letter
+    if(chars.length === 1) return chars;
     
-    const compressionMap = new Map()
-
-// We iterate over the provided array, chars.
-// For each unique character, we find the number of times it repeats consecutively.
-
-    for( c of chars){
-        if(compressionMap.has(c)){
-            compressionMap.set(c, compressionMap.get(c) + 1)
-        } else {
-            compressionMap.set(c,1)
-        }
-    }    
-
-    // We then overwrite the original chars array with the character followed by the number of occurrences if more than one.
-    let j =0;
-    for(c of compressionMap){
-        // console.log(c)
-     if(c[1]===1){
-        s+= c[0]
-     } else {
-        s = s + c[0] + c[1]
-     }
+    let s ="";
+    let group = chars[0]
+    let currenGroupCount = 1; 
+    
+    for(let i = 0; i<=chars.length;i++){
+        console.log(chars[i])
+        // Start with the second letter & check if they are of the same group or new group
+        // If new group add current group letter and count to s and initalise new group
+        // if final character add to s
     }
-    // console.log("current s:", s)
 
-    for(c in s){
-        console.log(c)
-        chars[c] = s[c]
-    }
-    chars.length = s.length;
-    console.log("final chars",chars)
 
-    // We have to return the length of the array after the modifications.
-    // console.log("chars result",chars)
-    return chars.length;
+    // Compress array to resulting array length
+    chars.length = s.length; 
+
+    // Create for loop that appends compressed result  
+    return chars.length; 
 };
 
 // const result = compress(['a','a','b','b','c','c','c'])
@@ -63,7 +108,3 @@ const result = compress(["a","a","a","b","b","a","a"])
 
 console.log(result)
 
-
-
-// Expected
-// ["a","b","1","2"]
