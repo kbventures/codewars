e6887414d473d01147b79986ec67d085e36163fb
kbventures <kmb_x@hotmail.com>
2024-10-22 08:38:15 -0500

    Leetcode Binary Search GuessNumberHigherOrLower

--- Leetcode/BinarySearch/index.js
+++ Leetcode/BinarySearch/index.js
@@ -123,10 +123,10 @@
 var search = function(nums, target) {
 let left = 0;
 let right = nums.length-1;
-let middle; 
+
 
 while(left <= right){
-    middle = Math.floor((left+right)/2)
+    const middle = Math.floor((left+right)/2)
 
     if(nums[middle] == target){
         return middle; 
