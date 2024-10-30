4bba414d0723a0dd60718a2dbd402ff494168499
kbventures <kmb_x@hotmail.com>
2024-10-30 12:14:53 -0500

    Leetcode Symmetric Tree

---
+++ Leetcode/Trees/MaxDepthBinaryTree/index.js
@@ -0,0 +1,71 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+// 104. Maximum Depth of Binary Tree
+// Easy
+// Topics
+// Companies
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+2
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+// Example 2:
+
+// Input: root = [1,null,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 104].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+
+    class Node {
+        constructor(value){
+            this.value = value;
+            this.left = null;
+            this.right = null; 
+        }
+    }
+    
+    class Tree{
+        constructor(){
+            this.root = null;  
+        }
+    
+        isEmpty(){
+            return this.root == null;
+        }
+    
+        search(){
+            // dfs vs bfs
+        }
+    }
+    
+    const newTree = new Tree()
+
+
+    
+        // Recursively go left and right until all nodes are searched
+
+    
+};
