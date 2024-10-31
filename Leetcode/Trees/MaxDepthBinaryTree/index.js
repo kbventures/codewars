10b8c40a5fed30243655306903ab6b7d13fac9ba
kbventures <kmb_x@hotmail.com>
2024-10-31 10:10:40 -0500

    Leetcode Minimum Depth Of a Binary Tree

--- Leetcode/Trees/MaxDepthBinaryTree/index.js
+++ Leetcode/Trees/MaxDepthBinaryTree/index.js
@@ -39,33 +39,11 @@
  */
 var maxDepth = function(root) {
 
-    class Node {
-        constructor(value){
-            this.value = value;
-            this.left = null;
-            this.right = null; 
-        }
-    }
-    
-    class Tree{
-        constructor(){
-            this.root = null;  
-        }
-    
-        isEmpty(){
-            return this.root == null;
-        }
-    
-        search(){
-            // dfs vs bfs
-        }
-    }
-    
-    const newTree = new Tree()
+    if(!root) return 0; 
 
+    const left = maxDepth(root.left)
+    const right = maxDepth(root.right)
 
-    
-        // Recursively go left and right until all nodes are searched
-
+    return 1 + Math.max(left, right)
     
 };
