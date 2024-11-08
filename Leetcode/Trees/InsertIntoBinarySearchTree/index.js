eb0ab973e72364622240ab582707bae519f82f30
kbventures <kmb_x@hotmail.com>
2024-11-08 16:18:25 -0500

    Leetcode Binary Search Tree Insert Solution

--- Leetcode/Trees/InsertIntoBinarySearchTree/index.js
+++ Leetcode/Trees/InsertIntoBinarySearchTree/index.js
@@ -1,14 +1,4 @@
 // https://leetcode.com/problems/insert-into-a-binary-search-tree/
-
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -30,24 +20,19 @@ var insertIntoBST = function(root, val) {
             this.right = null; 
             }
     }
-
-
-// Scenario One No Node: Insert Node with Value
-// Scenario Two One Only One Node(itself): replace with value and create left or right node or create left or right node
-// If there is at least one branch we traverse that branch until we find a suitable place in the tree
-
     function iIaBsT(r,v){
         // base case
-        if(r == null) return new Node(v)
-
-        if(r.left && r.val > val){
-                r.left = iIaBsT(r,v)
-        } else if ( r.right && r.val < val){
-                r.right = iIaBsT(r,v) 
+        if(r === null) {
+            return new Node(v)
+        }
+        if(val < r.val){
+                r.left = iIaBsT(r.left,v)
+        } else if (val > r.val){
+                r.right = iIaBsT(r.right,v) 
             
         }
         return r; 
     }
+    
     return iIaBsT(root,val) 
-
 };
