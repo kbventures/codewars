6e2ed6156b8177a6c76df5f4b956f7207a255163
kbventures <kmb_x@hotmail.com>
2024-11-08 11:27:10 -0500

    Leetcode binary search tree insert v2

--- Leetcode/Trees/InsertIntoBinarySearchTree/index.js
+++ Leetcode/Trees/InsertIntoBinarySearchTree/index.js
@@ -8,6 +8,15 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
 /**
  * @param {TreeNode} root
  * @param {number} val
@@ -22,25 +31,23 @@ var insertIntoBST = function(root, val) {
             }
     }
 
-    // base case
-    if(val == null) return root; 
 
-    if(root.val < val && root.left == null){
-            const newNode = new Node(val)
-            root.left = newNode;  
-            val = null; 
-            return 
-    } else if (root.val > val && root.right == null ){
-        const newNode = new Node(val)
-            root.right = newNode;  
-            val = null; 
-            return 
-    }
+// Scenario One No Node: Insert Node with Value
+// Scenario Two One Only One Node(itself): replace with value and create left or right node or create left or right node
+// If there is at least one branch we traverse that branch until we find a suitable place in the tree
 
-    if(root.val > val && root.right){
-        return insertIntoBST(root.right,val)
-    } else if( root.val < val && root.lef){
-        return insertIntoBST(root.left,val)
+    function iIaBsT(r,v){
+        // base case
+        if(r == null) return new Node(v)
+
+        if(r.left && r.val > val){
+                r.left = iIaBsT(r,v)
+        } else if ( r.right && r.val < val){
+                r.right = iIaBsT(r,v) 
+            
         }
-   
+        return r; 
+    }
+    return iIaBsT(root,val) 
+
 };
