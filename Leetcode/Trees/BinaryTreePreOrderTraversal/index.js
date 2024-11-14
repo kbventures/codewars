b5d77e1ed2e36adbe611435e1b09adfadadc71ce
kbventures <kmb_x@hotmail.com>
2024-11-14 08:49:11 -0500

    Leetcode Binary Search Bree Pre Order Traversal

---
+++ Leetcode/Trees/BinaryTreePreOrderTraversal/index.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let result = []
+
+    function pT(r){
+        if(!r) return result; 
+        result.push(r.val)
+        pT(r.left)
+        pT(r.right)
+        return result; 
+    }
+    return pT(root)
+};
