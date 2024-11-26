24083d2aa388a9908a03751925fde4b4ce1397d5
kbventures <kmb_x@hotmail.com>
2024-11-26 09:02:28 -0500

    Leetcode Group Anagram Review

---
+++ Leetcode/Arrays_and_hashing/TopKFrequentElements/index.js
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/top-k-frequent-elements
+
+// 347. Top K Frequent Elements
+// Medium
+// Topics
+// Companies
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+// k is in the range [1, the number of unique elements in the array].
+// It is guaranteed that the answer is unique.
+ 
+
+// Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+    var topKFrequent = function(nums, k) {
+
+        let newHash = {}
+        for(let i = 0;i<nums.length;i++){
+            newHash[nums[i]] ? newHash[nums[i]]++ : newHash[nums[i]] = 1; 
+            console.log("result", newHash)
+        }
+        let result = []
+        for(let z in newHash){
+            result.push(newHash[z])
+        }
+         
+        result = result.sort()
+        let result1 = result[result.length-1]
+        let result2 = result[result.length-2]
+
+        return [result1,result2]
+    };
+
+
+const result = topKFrequent([1,1,1,2,2,3],2)
+
+console.log(result)
