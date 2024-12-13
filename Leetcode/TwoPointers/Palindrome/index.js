5ef4e88d5ed4c7846f09c5bee5f854dfe1e349b0
kbventures <kmb_x@hotmail.com>
2024-12-13 09:43:41 -0500

    Leetcode valid palindrome v2 optimised brute force

--- Leetcode/TwoPointers/Palindrome/index.js
+++ Leetcode/TwoPointers/Palindrome/index.js
@@ -24,9 +24,20 @@ var isPalindrome = function(s) {
     return sanitized == sanitized.split('').reverse().join('')
 };
 
-
-// V2 Two Pointers
-
+// V2 Optimised Brute Froce 
 var isPalindrome = function(s) {
-   
+    // remove spaces
+    let sanitized = ""
+    let revSanitized=""
+    for(let i =s.length-1; i>=0;i--){
+        // check for numberes between 48 and 57 inclussive ascii or 97-122 inclusive for lower case letters
+        let charCodecheck = s[i].toLowerCase().charCodeAt()
+        if((charCodecheck > 47 && charCodecheck < 58)||(charCodecheck > 96 && charCodecheck < 123)){
+            revSanitized = revSanitized + s[i].toLowerCase()
+            sanitized =  s[i].toLowerCase() + sanitized;
+        }
+    }
+    if(s.length == 0) return false; 
+    return sanitized == revSanitized
 };
+
