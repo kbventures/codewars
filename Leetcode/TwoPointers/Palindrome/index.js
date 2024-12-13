cf7d6673433598cd2bc7e94286af4f81c699cb13
kbventures <kmb_x@hotmail.com>
2024-12-13 09:12:02 -0500

    Leetcode Valid Palindrome review v1 brute force

--- Leetcode/TwoPointers/Palindrome/index.js
+++ Leetcode/TwoPointers/Palindrome/index.js
@@ -1,18 +1,32 @@
 // https://leetcode.com/problems/valid-palindrome/
 
+// V1 Brute force
+
 var isPalindrome = function(s) {
     // remove spaces
     let sanitized = ""
     for(let i =s.length-1; i>=0;i--){
+
+        // check for numberes between 48 and 57 inclussive ascii 
+        let charCodeNumCheck = s[i].charCodeAt()
+
+
+        // This Checks for letters but not numbers
         let charCodeAt = s[i].toLowerCase().charCodeAt() - 96
         if(charCodeAt < 27 && charCodeAt > 0){
             sanitized =  s[i].toLowerCase() + sanitized;
 
+        }  else if (charCodeNumCheck > 47 && charCodeNumCheck < 58){
+            sanitized = s[i] + sanitized; 
         }
     }
-
-    conosle.log("0".charCodeAt(), "9".charCodeCat())
-    console.log(sanitized)
     if(s.length == 0) return false; 
     return sanitized == sanitized.split('').reverse().join('')
+};
+
+
+// V2 Two Pointers
+
+var isPalindrome = function(s) {
+   
 };
