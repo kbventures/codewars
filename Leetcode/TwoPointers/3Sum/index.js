005626a85c409b309abbe005eb90326c58d63b7c
kbventures <kmb_x@hotmail.com>
2024-12-18 12:12:54 -0500

    Leetcode Two Pointerse 3Sum

--- Leetcode/TwoPointers/3Sum/index.js
+++ Leetcode/TwoPointers/3Sum/index.js
@@ -4,7 +4,6 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
     var threeSum = function(nums) {
         let sortedNums = nums.sort((a,b)=>a-b)
         let result = []
@@ -20,6 +19,36 @@ var threeSum = function(nums) {
         }
         return result; 
     };
-};
+
+    // solution
+    var threeSum = function(nums) {
+        let sortedNums = nums.sort((a, b) => a - b);
+        let result = [];
+        
+        for (let i = 0; i < nums.length; i++) {
+            if(nums[i]>0)break
+            if (i > 0 && sortedNums[i] === sortedNums[i - 1]) continue; // Skip duplicates
+            
+            let left = i + 1;
+            let right = nums.length - 1;
+            
+            while (left < right) {
+                let tempCheck = sortedNums[i] + sortedNums[left] + sortedNums[right];
+                
+                if (tempCheck === 0) {
+                    result.push([sortedNums[i], sortedNums[left], sortedNums[right]]);
+                    left++;
+                    right--;
+                    while (left < right && sortedNums[left] === sortedNums[left - 1]) left++; // Skip duplicates
+                    while (left < right && sortedNums[right] === sortedNums[right + 1]) right--; // Skip duplicates
+                } else if (tempCheck > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return result;
+    };
 
 
