b3bdf5b9fab10f7c3aa004e2933b317e028fa880
kbventures <kmb_x@hotmail.com>
2024-12-19 13:50:49 -0500

    Leetcode 3sum review

--- Leetcode/TwoPointers/3Sum/index.js
+++ Leetcode/TwoPointers/3Sum/index.js
@@ -22,11 +22,29 @@
 
     // solution
     var threeSum = function(nums) {
+        // We sort the numbers in order to be able to perform a 2 pointer to find two digits
+        // In the two pointer 2sum we move the pointers on the right left until the target sum is equal or less to the targer
+        // and if the right pointer and left pointer value sum is smaller than the target we move the left pointer to the right and so on
+        // so forth which solves two numbers. 
         let sortedNums = nums.sort((a, b) => a - b);
+        //Input: nums = [-4,-1,-1,0,1,2]
+
+
+        // this will hold the unique combinations
         let result = [];
         
+     // We fix the first number and perform a two pointer on the other two values;
         for (let i = 0; i < nums.length; i++) {
+
+            // if the fixed number is positive we can break out of the look and return []
+            // because it's not possible to obtain 0 with positive numbers
+            // we know they will all be positive numbers after because they are now sorted
+            // this also serves to break out early if there no possible solutions and improves
+            // performance avoiding uncessary checks. 
             if(nums[i]>0)break
+
+            // Once we have checked the first value we can start skipping duplicate values by
+            // comparing the current value and the previous
             if (i > 0 && sortedNums[i] === sortedNums[i - 1]) continue; // Skip duplicates
             
             let left = i + 1;
