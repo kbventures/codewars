c183c69a22310384a5c21e8ed3edb5800828834f
kbventures <kmb_x@hotmail.com>
2024-12-30 13:46:37 -0500

    Leetcode sliding widow best time to sell stock

--- Leetcode/SlidingWindow/BestTimeTobuyAndSellStock/index.js
+++ Leetcode/SlidingWindow/BestTimeTobuyAndSellStock/index.js
@@ -4,24 +4,41 @@
  * @param {number[]} prices
  * @return {number}
  */
+
+// Brute force time O(n^2) space O(1)
+// var maxProfit = function(prices) {
+// let profit =0; 
+// for(let i =0; i<prices.length;i++){
+//     for(let y=prices.length-1; y > i;y--){
+//         if(prices[i]< prices[y]){
+//             let tempProfit = prices[y] - prices[i]
+//             if(profit < tempProfit){
+//                 profit = tempProfit
+//                 }
+//         }
+//     }
+// }
+//     return profit; 
+// };
+
+// O(n) ???
 var maxProfit = function(prices) {
-
-
-    // [7,1,5,3,6,4]
-    
-    Window 0,1
-    Buy @ 7 sell @ 1 loss -6
-    Window 1,2
-    Buy @ 1 sell @ 5 gain 5
-    Window 1,3
-    Buy @ 1 sell @ 3 gain 2
-    Window 1,4
-    Buy @ 1 sell @ 6 gain 5
-    Window 1,5
-    Buy @ 1 sell @ 4 gain 3; 
+    let profit =0; 
+    let i =0; 
+    let y = prices.length-1;
     
     
-    
-    
+    // You need ot keep track of the lowest price
+    while(i<y){
+        if(prices[i]<prices[y]){
+            let tempProfit = prices[y]-prices[i]
+            if(tempProfit > profit){
+                profit = tempProfit
+            }
+            y--
+        } else{
+            i++
+        }
+    }
         return profit; 
     };
