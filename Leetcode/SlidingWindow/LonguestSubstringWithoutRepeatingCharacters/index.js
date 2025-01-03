6869b39b33e9c81cd9073c5aa1002dbda53f6ec9
kbventures <kmb_x@hotmail.com>
2025-01-03 18:10:11 -0500

    Leetcode Sliding Window Longuest Substring Without Repeating Characters

---
+++ Leetcode/SlidingWindow/LonguestSubstringWithoutRepeatingCharacters/index.js
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let currStr = ""
+    let maxStr = 0; 
+    let newSet = {};
+    for(one of s){
+        console.log("one",one)
+        console.log("currStr",currStr)
+        console.log("maxStr",maxStr)
+        console.log("newSet",newSet)
+        console.log("******")
+        if(newSet[one]){
+            // Reset
+            newSet = {}
+            if(currStr.length > maxStr){
+                maxStr = currStr.length; 
+            }
+            currStr = "";
+            currStr += one;
+            newSet[one]= "exists"; 
+ 
+        } else {
+            // add 
+            newSet[one]= "exists"; 
+            currStr += one; 
+        }
+        console.log("one",one)
+        console.log("currStr",currStr)
+        console.log("maxStr",maxStr)
+        console.log("newSet",newSet)
+        console.log("******")
+    }
+        console.log("end")
+        console.log("one",one)
+        console.log("currStr",currStr)
+        console.log("maxStr",maxStr)
+        console.log("newSet",newSet)
+        console.log("******")
+
+    return maxStr; 
+};
