46ac67a1d9d697391570f7dba727b1a6b5d74121
kbventures <kmb_x@hotmail.com>
2025-01-16 12:17:55 -0500

    Leetcode

---
+++ Leetcode/SlidingWindow/LongestRepeatingCharacterReplacement/index.ts
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/longest-repeating-character-replacement/
+function characterReplacement(s: string, k: number): number {
+    let hash: Record<string,number>={}; 
+
+
+    return 0;
+};
