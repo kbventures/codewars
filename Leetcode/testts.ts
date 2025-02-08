72b5adc944644fd92026f50417a8a9a2c858d7f8
kbventures <kmb_x@hotmail.com>
2025-02-08 08:48:13 -0500

    Successful MinStack Linked Stack Implementation Attempt

---
+++ Leetcode/testts.ts
@@ -0,0 +1,73 @@
+// Node based stack attemp
+
+class StackNode{
+    data: number | string;
+    next: null | StackNode 
+    constructor(data: number | string){
+        this.data = data;
+        this.next = null; 
+    }
+}
+
+class StackClass{
+    private top: null | StackNode 
+    private size: number;
+    constructor(){
+        this.top = null; 
+        this.size = 0; 
+    }
+    push(data){
+        let newNode = new StackNode(data)
+        newNode.next = this.top;
+        this.top = newNode;
+        this.size++
+    }
+    pop(){
+        if(this.top){
+        this.top = this.top.next;
+        this.size--
+        } else {
+            console.log("Stack is empty!")
+        }
+    }
+    peek(){
+       if(this.top) {
+        return this.top;
+       } else {
+        console.log("Stack is empty!")
+       }
+    }
+    length(){
+return this.size       
+    }
+
+
+}
+
+
+
+
+const newStackClass = new StackClass()
+newStackClass.length()
+newStackClass.push("Test 0");
+console.log(newStackClass)
+newStackClass.push("Test 1");
+console.log(newStackClass)
+newStackClass.length()
+newStackClass.push("Test 2");
+console.log(newStackClass)
+newStackClass.push("Test 3");
+newStackClass.length()
+newStackClass.peek()
+console.log(newStackClass)
+newStackClass.peek()
+newStackClass.pop()
+console.log(newStackClass)
+newStackClass.pop()
+console.log(newStackClass)
+newStackClass.pop()
+console.log(newStackClass)
+newStackClass.pop()
+console.log(newStackClass)
+newStackClass.pop()
+console.log(newStackClass)
