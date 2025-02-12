f7519b0882379b7d6e46dfdeb804388a289a1a78
kbventures <kmb_x@hotmail.com>
2025-02-12 15:17:38 -0500

    Leetcode Daily Tempatures Monotonic Decreasing Stack Solution

--- Leetcode/Stack/DailyTemperatures/index.js
+++ Leetcode/Stack/DailyTemperatures/index.js
@@ -2,16 +2,40 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-const dailyTemperatures = (temperatures) => {
-	const waitTimes = temperatures.map((currentTemperature, index) => {
-		let daysPast = 0
-		for (let i = index; i < temperatures.length; i++) {
-			if (currentTemperature < temperatures[i]) {
-				return daysPast
-			}
-			daysPast++
-		}
-		return 0
-	})
-	return waitTimes
-}
+// Brute force O(n^2)
+//  const dailyTemperatures = (temperatures) => {
+// 	const waitTimes = temperatures.map((currentTemperature, index) => {
+// 		let daysPast = 0
+// 		for (let i = index; i < temperatures.length; i++) {
+// 			if (currentTemperature < temperatures[i]) {
+// 				return daysPast
+// 			}
+// 			daysPast++
+// 		}
+// 		return 0
+// 	})
+// 	return waitTimes
+// }
+
+
+// Space complexity O(n) and time complexity O(1) utilizing monotonic decreasing stack: 
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let result = new Array(temperatures.length).fill(0);
+    let temperatureStack = [];
+
+    for (let i = 0; i < temperatures.length; i++) {
+        while (temperatureStack.length > 0 && temperatures[i] > temperatures[temperatureStack[temperatureStack.length - 1]]) {
+            let index = temperatureStack.pop();
+            result[index] = i - index;
+        }
+        temperatureStack.push(i);
+    }
+
+    return result;
+};
+
