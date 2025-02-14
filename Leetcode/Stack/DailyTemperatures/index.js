5f897cb8fdc66dd61a8230b2663910f7cc05164e
kbventures <kmb_x@hotmail.com>
2025-02-14 10:05:51 -0500

    Leetcode daily temperatures decreasing monotonic stack version

--- Leetcode/Stack/DailyTemperatures/index.js
+++ Leetcode/Stack/DailyTemperatures/index.js
@@ -2,40 +2,44 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-// Brute force O(n^2)
-//  const dailyTemperatures = (temperatures) => {
-// 	const waitTimes = temperatures.map((currentTemperature, index) => {
-// 		let daysPast = 0
-// 		for (let i = index; i < temperatures.length; i++) {
-// 			if (currentTemperature < temperatures[i]) {
-// 				return daysPast
-// 			}
-// 			daysPast++
-// 		}
-// 		return 0
-// 	})
-// 	return waitTimes
-// }
-
-
-// Space complexity O(n) and time complexity O(1) utilizing monotonic decreasing stack: 
-
+ // Brute force
+//  var dailyTemperatures = function(temperatures) {
+//     const result = temperatures.map((el,idx)=>{
+//         for(let i=idx+1;i<temperatures.length;i++){
+//             if(temperatures[i] > el){
+//                 return i - idx;
+//             }
+//         }
+//         return 0; 
+//     })
+//     return result; 
+// };
 /**
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-    let result = new Array(temperatures.length).fill(0);
-    let temperatureStack = [];
+ // Monotonic stack
+ var dailyTemperatures = function(temperatures) {
+let stack = []
+let result = []
 
-    for (let i = 0; i < temperatures.length; i++) {
-        while (temperatureStack.length > 0 && temperatures[i] > temperatures[temperatureStack[temperatureStack.length - 1]]) {
-            let index = temperatureStack.pop();
-            result[index] = i - index;
-        }
-        temperatureStack.push(i);
+for(let i =0; i<temperatures.length;i++){
+   if(stack.length==0){
+    stack.push([temperatures[i],i])
+   } else if(stack[stack.length-1] < temperatures[i]) {
+    //  Empty stack and update positions relative to higher tempeatures in result array
+    while(){
+        
     }
+   } else {
 
-    return result;
+   }
+}
+return result; 
 };
+
+
+
+
+
 
