924bda94f6a816583b638d7aa6f70a5f562d768e
kbventures <kmb_x@hotmail.com>
2025-03-19 07:55:22 -0500

    Simplified version

--- Leetcode/BinarySearch/BinarySearch/index.ts
+++ Leetcode/BinarySearch/BinarySearch/index.ts
@@ -1,26 +1,15 @@
+// Time compleity: O(log n)
+// Space complexity: O(1)
 function search(nums: number[], target: number): number {
-    // Edge case length is 1
-    if(nums.length == 1){
-        return nums[0] == target ? 0 : -1
+    let start = 0, end = nums.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if (nums[mid] === target) return mid;  // Direct return on match
+        if (nums[mid] < target) start = mid + 1;  // Update start
+        else end = mid - 1;  // Update end
     }
 
-    let start = 0; 
-    let end = nums.length - 1 
-    let mid = Math.floor((start + end) / 2)
-
-    while(start <= end){
-        // Check if number found and if found return index
-        if(nums[mid] == target){
-            return mid
-        } else if (nums[mid] < target) {
-            start = mid + 1
-        } else {
-            // nums[mid] > target
-            end = mid -1
-        }
-        mid = Math.floor((start + end) / 2)
-
-    }
-    // Default result
-    return -1
-};
+    return -1;  // Target not found
+}
