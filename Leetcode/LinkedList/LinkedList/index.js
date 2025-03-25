9e05a8ad65908bb31d559c5ff9819a2642715012
kbventures <kmb_x@hotmail.com>
2025-03-25 08:39:36 -0400

    Leetcode javascript linkedlist implementation

---
+++ Leetcode/LinkedList/LinkedList/index.js
@@ -0,0 +1,42 @@
+
+class ListNode {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+    }
+  
+    append(value) {
+      const newNode = new ListNode(value);
+      if (!this.head) {
+        this.head = newNode;
+        return;
+      }
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  
+    print() {
+      let current = this.head;
+      while (current) {
+        console.log(current.value);
+        current = current.next;
+      }
+    }
+  }
+  
+  // Usage
+  const list = new LinkedList();
+  list.append(10);
+  list.append(20);
+  list.append(30);
+  list.print();
+  
