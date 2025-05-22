737d9371087734dd4d34ad24c842b1b74777724f
kbventures <kmb_x@hotmail.com>
2025-05-22 11:55:09 -0400

    Leetcode ImPlement Cyclic Undedirected Graph

--- Leetcode/Graphs/ImplementGraph/index.ts
+++ Leetcode/Graphs/ImplementGraph/index.ts
@@ -1,30 +1,37 @@
-class Node {
-    val: number;
-    neighbors: Node[];
+// Undirected cyclic graph implementation:
 
-    constructor(val?: number, neighbors?: Node[]) {
-        this.val = val === undefined ? 0 : val;
-        this.neighbors = neighbors === undefined ? [] : neighbors;
+class _Node {
+    val: number;
+    neighbors: _Node[]
+
+    constructor(val?:number, neighbors?: _Node[]){
+        this.val = ( val === undefined ? 0 : val)
+        this.neighbors = ( neighbors === undefined ? [] : neighbors)
     }
 }
-const node1 = new Node(1);
-const node2 = new Node(2);
-const node3 = new Node(3);
-const node4 = new Node(4);
+
+
+const node1 = new _Node(1);
+const node2 = new _Node(2);
+const node3 = new _Node(3);
+const node4 = new _Node(4);
 
 node1.neighbors.push(node2, node4);
-node2.neighbors.push(node1, node3);
+node2.neighbors.push(node1,node3);
 node3.neighbors.push(node2, node4);
 node4.neighbors.push(node1, node3);
 
+console.log(node1);
 
-function dfs(node: Node, visited = new Set<Node>()) {
-    if (!node || visited.has(node)) return;
-    console.log(`Visiting node ${node.val}`);
-    visited.add(node);
-    for (let neighbor of node.neighbors) {
-        dfs(neighbor, visited);
+// used to traverse graph
+function dfs(originalNode: _Node, visited = new Set<_Node>()): void{
+    if(visited.has(originalNode)) return
+    console.log("Visiting: ", originalNode)
+    visited.add(originalNode)
+    for( let neighbor of originalNode.neighbors){
+        dfs(neighbor, visited)
     }
 }
 
-dfs(node1);
+
+dfs(node1)
